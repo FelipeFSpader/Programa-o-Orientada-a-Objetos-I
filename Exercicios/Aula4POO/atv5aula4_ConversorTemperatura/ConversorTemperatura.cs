5ad6f5d7999f4e68eb09d2bde1b6bcf05cc93631
Luís Felipe <luisfelipespader@gmail.com>
2026-08-30 22:02:11 -0300

    número 5 ainda meia bomba

--- Exercicios/Aula4POO/atv5aula4_ConversorTemperatura/ConversorTemperatura.cs
+++ Exercicios/Aula4POO/atv5aula4_ConversorTemperatura/ConversorTemperatura.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace QuintaAtividade;
+
+public class ConversorTemperatura
+{
+    //aqui vem metodos
+
+    // Celsius para Fahrenheit: F=(C×1.8)+32
+    public double CelciusParaFh()
+    {
+        
+    }
+    
+    //Fahrenheit para Celsius: C = (F-32)/1.8
+    public double FhParaCelcius()
+    {
+        
+    }
+
+
+
+}
